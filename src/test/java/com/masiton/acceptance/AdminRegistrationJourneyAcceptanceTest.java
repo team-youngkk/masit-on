7b6001a0269417bc3ed082e3e7abe33f827e86a6
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-07-28 15:34:15 +0900

    test: 인수 테스트 JPA 운영 설정 적용

--- src/test/java/com/masiton/acceptance/AdminRegistrationJourneyAcceptanceTest.java
+++ src/test/java/com/masiton/acceptance/AdminRegistrationJourneyAcceptanceTest.java
@@ -31,7 +31,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.jpa.open-in-view=false",
+        "spring.jpa.hibernate.ddl-auto=validate"
+})
 @AutoConfigureMockMvc
 @Testcontainers
 @DisplayName("관리자 데이터 등록 사용자 여정 인수")
