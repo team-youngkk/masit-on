227de323685547c65f1530c02e6510bb44143cde
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-08-28 11:06:28 +0900

    fix: allow sitemap crawling in robots policy

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -19,7 +19,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/restaurants', '/restaurants/'],
+        allow: ['/restaurants', '/restaurants/', '/sitemap.xml'],
         disallow: '/',
       },
     ],
