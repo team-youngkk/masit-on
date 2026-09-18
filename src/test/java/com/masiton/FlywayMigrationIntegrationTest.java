58b96639c91fa85272c2a9006a8cb60a507507b1
rladl <rladlsdks12@gmail.com>
2026-09-18 20:27:38 +0900

    test: retain constraint exception assertions

--- src/test/java/com/masiton/FlywayMigrationIntegrationTest.java
+++ src/test/java/com/masiton/FlywayMigrationIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
